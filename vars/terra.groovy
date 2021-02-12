78a6a98a84d9d15a605592cc6102ca8b2bc1912a
Bibek Shrestha <bibek.shrestha23@gmail.com>
2021-02-12 13:33:42 -0600

    my-shared-lib

---
+++ vars/terra.groovy
@@ -0,0 +1,24 @@
+def call() {
+    pipeline {
+    agent any
+
+    stages { 
+    stage('Terraform Init') {
+            steps {
+    sh '''cd test
+    terraform init'''
+        
+            }
+        }
+        
+     stage('Terraform Apply') {
+            steps {
+    sh '''cd test
+    terraform apply -auto-approve'''
+           
+                
+                }
+            }
+        }
+    }
+}
